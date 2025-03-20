4dd731f73a012509b0ead0afdf90ef39c4eb58ba
Kyle Tennison <135751865+kyle-tennison@users.noreply.github.com>
2025-03-19 21:40:52 -0700

    feat: header shadow (#7)

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,14 +1,42 @@
 import "../styles/header.css";
 import Logo from "../components/logo";
+import { useEffect, useRef } from "react";
 
 const PAYPAL_LINK =
   "https://www.paypal.com/donate/?business=54HEQEQEAT2M8&no_recurring=0&item_name=Help+me+continue+making+open-source+projects&currency_code=USD";
 const GITHUB_LINK =
   "https://gist.github.com/kyle-tennison/491123cd45b2c4c4afd7af3c9cd26f55";
 
+const SCROLL_THRESHOLD = 150;
+
 export default function Header() {
+  const headerElement = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      console.debug(window.scrollY);
+      if (window.scrollY > SCROLL_THRESHOLD) {
+        if (headerElement.current) {
+          // headerElement.current.style.opacity = "0"
+          headerElement.current.style.boxShadow =
+            "0px 4px 8px rgba(0, 0, 0, 0.10)";
+        }
+      } else {
+        if (headerElement.current) {
+          // headerElement.current.style.opacity = "1"
+          headerElement.current.style.boxShadow = "none";
+        }
+      }
+    });
+  }, []);
+
   return (
-    <div className="header">
+    <div
+      className="header"
+      ref={(el) => {
+        headerElement.current = el;
+      }}
+    >
       <div className="logo-container">
         <Logo />
         <a className="logo-font dark-cream">Ragposium</a>
